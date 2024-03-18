d19cc560c9b73ba2ac761c2528d6b01a93927385
megaitsong <nana@naver.com>
2024-03-18 18:10:48 +0900

    no message

---
+++ src/main/java/com/syj/spgram/SpgramApplication.java
@@ -0,0 +1,16 @@
+package com.syj.spgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SpgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syj/spgram/SpgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syj.spgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
